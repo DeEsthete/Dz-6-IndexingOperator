569f46f73dbde178102d3777557e89c9201c0db5
Jeka Lion <djoni-01@outlook.com>
2017-12-05 22:29:09 +0600

    Добавьте файлы проекта.

---
+++ IndexingOperator/IArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexingOperator
+{
+    class IArray
+    {
+        public double[] Array;
+        public double this[int index]
+        {
+            get
+            {
+                return Array[index];
+            }
+
+            set
+            {
+                Array[index] = Math.Pow(value, 2);
+            }
+        }
+        public IArray(int rangeOfFinish)
+        {
+            Array = new double[rangeOfFinish];
+        }
+    }
+}
---
+++ IndexingOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexingOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IArray ar = new IArray(3);
+            ar.Array[0] = 2;
+            ar.Array[1] = 4;
+            ar.Array[2] = 8;
+            Console.WriteLine(ar.Array[0]);
+            Console.WriteLine(ar.Array[1]);
+            Console.WriteLine(ar.Array[2]);
+            Console.ReadLine();
+        }
+    }
+}
